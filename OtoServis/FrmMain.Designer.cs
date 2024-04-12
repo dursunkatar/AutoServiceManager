92252e2e8984d4a5df4c0562a61a1fbd597fb4ec
dursunkatar <dursun.katar@gmail.com>
2024-04-12 16:47:54 +0300

    .

--- OtoServis/FrmMain.Designer.cs
+++ OtoServis/FrmMain.Designer.cs
@@ -33,8 +33,23 @@
             label1 = new Label();
             pictureBox2 = new PictureBox();
             label2 = new Label();
+            pictureBox3 = new PictureBox();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox5 = new PictureBox();
+            label6 = new Label();
+            pictureBox6 = new PictureBox();
+            label7 = new Label();
+            pictureBox7 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
             SuspendLayout();
             // 
             // pictureBox1
@@ -42,7 +57,7 @@
             pictureBox1.BorderStyle = BorderStyle.FixedSingle;
             pictureBox1.Cursor = Cursors.Hand;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(24, 29);
+            pictureBox1.Location = new Point(6, 7);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(217, 202);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -55,7 +70,7 @@
             label1.BorderStyle = BorderStyle.FixedSingle;
             label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
             label1.ForeColor = Color.Black;
-            label1.Location = new Point(24, 208);
+            label1.Location = new Point(6, 186);
             label1.Name = "label1";
             label1.Size = new Size(217, 23);
             label1.TabIndex = 1;
@@ -67,7 +82,7 @@
             pictureBox2.BorderStyle = BorderStyle.FixedSingle;
             pictureBox2.Cursor = Cursors.Hand;
             pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-            pictureBox2.Location = new Point(247, 29);
+            pictureBox2.Location = new Point(229, 7);
             pictureBox2.Name = "pictureBox2";
             pictureBox2.Size = new Size(217, 202);
             pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -80,26 +95,170 @@
             label2.BorderStyle = BorderStyle.FixedSingle;
             label2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
             label2.ForeColor = Color.Black;
-            label2.Location = new Point(247, 208);
+            label2.Location = new Point(229, 186);
             label2.Name = "label2";
             label2.Size = new Size(217, 23);
             label2.TabIndex = 3;
             label2.Text = "TAMİR PANELİ";
             label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // pictureBox3
+            // 
+            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox3.Cursor = Cursors.Hand;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(452, 7);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(217, 202);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 4;
+            pictureBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.White;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(452, 186);
+            label3.Name = "label3";
+            label3.Size = new Size(217, 23);
+            label3.TabIndex = 5;
+            label3.Text = "PARÇA PANELİ";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox4.Cursor = Cursors.Hand;
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(6, 215);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(217, 202);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 6;
+            pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.White;
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(6, 394);
+            label4.Name = "label4";
+            label4.Size = new Size(217, 23);
+            label4.TabIndex = 7;
+            label4.Text = "SATIŞ PANELİ";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            label5.BackColor = Color.White;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(229, 394);
+            label5.Name = "label5";
+            label5.Size = new Size(217, 23);
+            label5.TabIndex = 9;
+            label5.Text = "PERSONEL PANELİ";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox5.Cursor = Cursors.Hand;
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(229, 215);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(217, 202);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.White;
+            label6.BorderStyle = BorderStyle.FixedSingle;
+            label6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(452, 394);
+            label6.Name = "label6";
+            label6.Size = new Size(217, 23);
+            label6.TabIndex = 11;
+            label6.Text = "ARAÇ PANELİ";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox6.Cursor = Cursors.Hand;
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(452, 215);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(217, 202);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 10;
+            pictureBox6.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.BackColor = Color.White;
+            label7.BorderStyle = BorderStyle.FixedSingle;
+            label7.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(675, 394);
+            label7.Name = "label7";
+            label7.Size = new Size(379, 23);
+            label7.TabIndex = 13;
+            label7.Text = "RAPORLAR";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox7.Cursor = Cursors.Hand;
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(675, 7);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(379, 410);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 12;
+            pictureBox7.TabStop = false;
+            // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(845, 437);
+            BackColor = Color.White;
+            ClientSize = new Size(1061, 423);
+            Controls.Add(label7);
+            Controls.Add(pictureBox7);
+            Controls.Add(label6);
+            Controls.Add(pictureBox6);
+            Controls.Add(label5);
+            Controls.Add(pictureBox5);
+            Controls.Add(label4);
+            Controls.Add(pictureBox4);
+            Controls.Add(label3);
+            Controls.Add(pictureBox3);
             Controls.Add(label2);
             Controls.Add(pictureBox2);
             Controls.Add(label1);
             Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
             Name = "FrmMain";
+            StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
             ResumeLayout(false);
         }
 
@@ -109,5 +268,15 @@
         private Label label1;
         private PictureBox pictureBox2;
         private Label label2;
+        private PictureBox pictureBox3;
+        private Label label3;
+        private PictureBox pictureBox4;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox5;
+        private Label label6;
+        private PictureBox pictureBox6;
+        private Label label7;
+        private PictureBox pictureBox7;
     }
 }
